94844885f0fbd6082befc7769a9ad40387e37423
umar482 <MacBook@macbook.home>
2017-04-29 17:26:50 +0500

    remove

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,1 @@
 @extends('layouts.app')
-
-hello this is mehar
-my first line mehar
-i am testing pull function in github mehar
-lets c what mehar
-
-umar branch test
-umar 1st line
-umar 2nd line
-umar 3rd line
-
