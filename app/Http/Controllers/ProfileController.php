2af55cc5b23fc4bc49a997f8ea5a9b00b40adffa
umairmehar <ddp-fa13-bcs-105@ciitlahore.edu.pk>
2017-04-30 18:30:03 +0500

    profile single only

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,85 +18,46 @@
         }
         public function createProfile(Request $request)
         {
-            if($request->user()->profile==null) {
-                $file = $request->file('profileImage');
-                $filename = $file->store('local');
-                $filecnic = $request->file('cnic');
-                $filenamecnic = $filecnic->store('local');
+            $file = $request->file('profileImage');
+            $filename = $file->store('local');
+            $filecnic = $request->file('cnic');
+            $filenamecnic = $filecnic->store('local');
 
-                $profile = new Profile();
-                $profile->bio = $request['bio'];
-                $profile->phone = $request->mobile;
-                $profile->picture = $filename;
-                $profile->cnic = $filenamecnic;
-                $adrs = new Address();
-                $adrs->city = $request->city;
-                $adrs->area = $request->area;
+            $profile = new Profile();
+            $profile->bio = $request['bio'];
+            $profile->phone = $request->mobile;
+            $profile->picture = $filename;
+            $profile->cnic = $filenamecnic;
+            $profile->city = $request->city;
+            $profile->area = $request->area;
+            $userid = $request->user()->id;
+            $isProfileExist = Profile::where('user_id',$userid)->first();
+            if($isProfileExist){
+                return view('home');
+            }
+//          $profile->save();
+            $request->user()->profile()->save($profile);
 
-    //        $profile->save();
-                $request->user()->profile()->save($profile)->address()->save($adrs);
+//          $adrs->save();
+//          $request->user()->profile()->address()->save($adrs);
 
-    //        $adrs->save();
-    //        $request->user()->profile()->address()->save($adrs);
+            if (is_array($request['language'])) {
+                foreach ($request['language'] as $lang) {
+                    $l = new Language();
+                    $l->language = $lang;
+//                  $l->save();
+                    $request->user()->profile()->orderBy('updated_at', 'desc')->first()->languages()->save($l);
 
-                if (is_array($request['language'])) {
-                    foreach ($request['language'] as $lang) {
-                        $l = new Language();
-                        $l->language = $lang;
-    //                $l->save();
-                        $request->user()->profile()->orderBy('updated_at', 'desc')->first()->languages()->save($l);
-
-                    }
-                }
-
-                if(is_array($request['Skill'])) {
-                    foreach ($request['Skill'] as $skill) {
-                        $sk = new Skill();
-                        $sk->skill = $skill;
-                        //                $sk->save();
-                        $request->user()->profile()->orderBy('updated_at', 'desc')->first()->skills()->save($sk);
-                        //
-                    }
-                }
-            }else if($request->user()->profile!=null){
-                    $file = $request->file('profileImage');
-                    $filename = $file->store('local');
-                    $filecnic = $request->file('cnic');
-                    $filenamecnic = $filecnic->store('local');
-                    Session::flash('message', 'This is a message!');
-                    $profile = new Profile();
-                    $profile->bio = $request['bio'];
-                    $profile->phone = $request->mobile;
-                    $profile->picture = $filename;
-                    $profile->cnic = $filenamecnic;
-                    $adrs = new Address();
-                    $adrs->city = $request->city;
-                    $adrs->area = $request->area;
-
-    //        $profile->save();
-                    $request->user()->profile()->update($profile)->address()->update($adrs);
-
-    //        $adrs->save();
-    //        $request->user()->profile()->address()->save($adrs);
-
-                    if(is_array($request['language'])){
-                        foreach ($request['language'] as $lang){
-                            $l = new Language();
-                            $l->language = $lang;
-    //                $l->save();
-                            $request->user()->profile()->orderBy('updated_at', 'desc')->first()->languages()->update($l);
-
-                        }
-                    }
-                    if(is_array($request['Skill'])){
-                        foreach ($request['Skill'] as $skill){
-                            $sk = new Skill();
-                            $sk->skill = $skill;
-    //                $sk->save();
-                            $request->user()->profile()->orderBy('updated_at', 'desc')->first()->skills()->update($sk);
-    //
-                        }
-                    }
                 }
             }
+
+            if(is_array($request['Skill'])) {
+                foreach ($request['Skill'] as $skill) {
+                    $sk = new Skill();
+                    $sk->skill = $skill;
+//                  $sk->save();
+                    $request->user()->profile()->orderBy('updated_at', 'desc')->first()->skills()->save($sk);
+                }
+            }
+        }
     }
